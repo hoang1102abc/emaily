f3625a2e3111ee5d97843548df5e6847796e2a6a
hoang1103 <hoang1103@users.noreply.github.com>
2020-06-14 22:43:01 +0100

    changed greeting

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ by: "there" });
 });
 
-const PORT = process.env.$PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT, "0.0.0.0");
