8b4a717b89eff0f3c5f2623b401a0e084c8c497d
hoang1103 <hoang1103@users.noreply.github.com>
2020-06-14 22:46:46 +0100

    changed greeting

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send({ by: "there" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "127.0.0.1";
 
 app.listen(PORT, "0.0.0.0");
